f457816599fbb4729514303daa6277a449ac5f11
Eirik Høyheim <eirik.hoyheim@nmbu.no>
2024-01-29 15:36:28 +0100

    non-linear run on orion

---
+++ run_in_thesis/non_linear_problem/layers/config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+config = {}
+config['num_epochs' ] = 2000
+config['n_nets'] = 10
+config['n_samples'] = 4*10**4
+config['n_layers'] = 4+2 # Two hidden + (one input+one output)
+# config['num_epochs' ] = 1000
+config['test_samples'] = 5000
+config['lr'] = 0.01
+config['num_transforms'] = 2  # For normalizing flows
+# config['hidden_dim'] = 600
+config['hidden_dim'] = 20 # Reduced to make it quicker to run simple checks
+
+config['verbose'] = True  # If we want printouts during/after training or not
+config['save_res'] = True  # If we should save the results
+config['patience'] = 300
