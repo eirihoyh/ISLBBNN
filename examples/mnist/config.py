0ed23fab336f9041e89df3f6d039c0b4057eeb30
Eirik Høyheim <eirik.hoyheim@nmbu.no>
2024-02-14 10:06:52 +0100

    longer run for mnist

--- examples/mnist/config.py
+++ examples/mnist/config.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 config = {}
-config['num_epochs' ] = 40  
-config['post_train_epochs'] = 40
+config['num_epochs' ] = 300  
+config['post_train_epochs'] = 100
 config['n_nets'] = 1  # Number of different networks to run for one problem
 config['class_problem'] = True  # If classification problem or not
-config['n_layers'] = 1+2 # Four hidden + (one input+one output)
+config['n_layers'] = 1+2 # One hidden + (one input+one output)
 config['lr'] = 0.05
 config['a_prior'] = 0.05
 config['num_transforms'] = 2  # For normalizing flows
