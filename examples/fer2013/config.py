9199918fb50384cf9c2d4396eb05bac8cf2d3231
Eirik Høyheim <eirik.hoyheim@nmbu.no>
2024-02-14 12:42:32 +0100

    ran longer example on fer2013 dataset

--- examples/fer2013/config.py
+++ examples/fer2013/config.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 
 config = {}
-config['num_epochs' ] = 40  
-config['post_train_epochs'] = 40
+config['num_epochs' ] = 1000  
+config['post_train_epochs'] = 200
 config['n_nets'] = 1  # Number of different networks to run for one problem
 config['class_problem'] = True  # If classification problem or not
 config['n_layers'] = 1+2 # Four hidden + (one input+one output)
-config['lr'] = 0.05
+config['lr'] = 0.005
 config['a_prior'] = 0.05
 config['num_transforms'] = 2  # For normalizing flows
-config['hidden_dim'] = 50 # Reduced to make it quicker to run simple checks
+config['hidden_dim'] = 100 # Reduced to make it quicker to run simple checks
 
 config['verbose'] = True  # If we want printouts during/after training or not
 config['save_res'] = False  # If we should save the results
