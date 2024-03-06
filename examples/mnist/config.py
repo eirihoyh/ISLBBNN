4977cee598a83f8396a7933464cd9129fbadae73
Eirik Høyheim <eirik.hoyheim@nmbu.no>
2024-03-06 14:04:42 +0100

    made new local explain plots for mnist

--- examples/mnist/config.py
+++ examples/mnist/config.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 config = {}
-config['num_epochs' ] = 300  
-config['post_train_epochs'] = 100
+config['num_epochs' ] = 600  
+config['post_train_epochs'] = 300
 config['n_nets'] = 1  # Number of different networks to run for one problem
 config['class_problem'] = True  # If classification problem or not
 config['n_layers'] = 1+2 # One hidden + (one input+one output)
