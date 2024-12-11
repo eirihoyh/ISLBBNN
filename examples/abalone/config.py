192258b7657f44751f1260c5cbf575d60e271562
Eirik Høyheim <eirik.hoyheim@nmbu.no>
2024-12-11 21:24:36 +0100

    included more examples and improved plots

--- examples/abalone/config.py
+++ examples/abalone/config.py
@@ -3,7 +3,7 @@
 
 config = {}
 config['num_epochs' ] = 5000  
-config['post_train_epochs'] = 1000
+config['post_train_epochs'] = 0 #1000
 config['n_nets'] = 1  # Number of different networks to run for one problem
 config['class_problem'] = False  # If classification problem or not
 config['n_layers'] = 2+2 # Four hidden + (one input+one output)
